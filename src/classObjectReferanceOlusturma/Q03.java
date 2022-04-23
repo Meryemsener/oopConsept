ca19ebe14d84a387e57d6b47d0816039119b7689
Meryem Aydin <meryemaydinsener@gmail.com>
2022-04-23 23:20:22 +0200

    new

---
+++ src/classObjectReferanceOlusturma/Q03.java
@@ -0,0 +1,14 @@
+package classObjectReferanceOlusturma;
+
+import java.util.Scanner;
+
+public class Q03 {
+    /*
+        Kullanıcıdan pozitif bir sayı girmesini isteyin ve asal olup olmadığını kontrol edin
+         */
+
+    public static void main(String[] args) {
+
+
+    }
+}
